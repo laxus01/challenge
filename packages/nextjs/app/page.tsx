459693c5e9291906f6720cbcbdd1a8a74ad531f3
jose.tunon@itxpertix.com <jose.tunon@itxpertix.com>
2024-08-09 10:43:41 -0500

    Se elimina el archivo Readme

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -3,8 +3,13 @@ import { MetisLogo } from "~~/components/assets/MetisLogo";
 
 const Home: NextPage = () => {
   return (
-    <>
-    </>
+    <div className="flex justify-center items-center flex-col flex-grow pt-10">
+      <div className="px-5 w-[90%] md:w-[75%]">
+        <div className="flex flex-col items-center justify-center text-accent">
+          <MetisLogo className="w-[240px]" />
+        </div>
+      </div>
+    </div>
   );
 };
 
